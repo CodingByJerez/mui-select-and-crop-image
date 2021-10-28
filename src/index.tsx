a5b7e37d2a35c16442b533ce618df07865c97304
Coding By Jerez <52014882+CodingByJerez@users.noreply.github.com>
2021-10-28 14:24:28 +0200

    fix: rename SelectAndCropImageDropAreaRef in ISelectAndCropImageDropAreaRef

--- src/index.tsx
+++ src/index.tsx
@@ -66,6 +66,6 @@ const SelectAndCropImage: FunctionComponent<IProps> = ({ ref, preview, image, hi
     </React.Fragment>
   );
 };
-export type { IDropAreaRef as SelectAndCropImageDropAreaRef };
+export type { IDropAreaRef as ISelectAndCropImageDropAreaRef };
 export { SelectAndCropImage, Provider as SelectAndCropImageProvider, RETURN_TYPE as SELECT_AND_CROP_IMAGE_RETURN_TYPE };
 export default SelectAndCropImage;
